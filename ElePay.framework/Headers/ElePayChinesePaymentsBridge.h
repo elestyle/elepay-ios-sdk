e4781812ae9e2f1a225b6e4b0bafdd58eea6623a
Xu Zhe <i@xuzhe.com>
2020-01-21 16:54:42 +0900

    Add directly WeChatPay support

--- ElePay.framework/Headers/ElePayChinesePaymentsBridge.h
+++ ElePay.framework/Headers/ElePayChinesePaymentsBridge.h
@@ -10,6 +10,15 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/*! @brief 错误码
+ *
+ */
+enum  ElePayWXErrCode: NSInteger {
+    ElePayWXSuccess         = 0,    /**< 成功*/
+    ElePayWXErrCodeOther    = -1,   /**< 普通错误类型*/
+    ElePayWXCancel          = -2,   /**< 用户点击取消并返回*/
+};
+
 @interface ElePayChinesePaymentsBridge : NSObject
 
 + (BOOL)isChinesePaymentsAvaliable;
@@ -17,6 +26,16 @@ NS_ASSUME_NONNULL_BEGIN
    onResult:(void(^)(NSDictionary * _Nullable resultDic))onResult;
 
 + (void)openURL:(NSURL *)url;
+
+// WeChat Pay Bridge
+
++ (BOOL)isWXAppInstalled;
++ (BOOL)isWXAppSupportApi;
+
++ (BOOL)registerWXApp:(NSString *)appid universalLink:(NSString *)universalLink;
++ (BOOL)handleWXOpenURL:(NSURL *)url;
++ (BOOL)handleWXOpenUniversalLink:(NSUserActivity *)userActivity;
++ (void)makeWXPayment:(NSDictionary *)params callback:(void(^)(NSInteger errCode, NSString *errMessage))onResult;
 
 @end
 
